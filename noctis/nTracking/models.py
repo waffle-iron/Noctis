ed5467404dc1719d9ff12cc4ec503139bd3a8b1d
prettytrue <mccartneyworks@gmail.com>
2016-03-08 04:31:47 +0000

    More work on the nTracking model. Adding simple fields from the web to make them usable by all apps. Going to work on the backbone understanding of status before migrating.

--- noctis/nTracking/models.py
+++ noctis/nTracking/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
-
 class nStatusType(models.Model):
     """
     All statuses have to have a type. This is there identifier. The
@@ -9,8 +8,14 @@ class nStatusType(models.Model):
     
     @param::name: The name of the status type
     @type::name: CharField
+
+    @param::status_breakdown: The actual breakdown that a status
+    goes through as it passes markers.
+    @type::status_breakdown: JSONField
     """
     name = models.CharField(max_length=150)
+    total_status_components = models.IntegerField(default=0)
+    status_breakdown = models.ListField(null=True)
 
     @python_2_unicode_compatible
     def __str__(self):
@@ -36,6 +41,17 @@ class nStatusComponent(models.Model):
 
     @param::status_type: The type od the status to be used
     @type::status_type: ForeignKey(nStatusType)
+
+    @param::status_level: The completion amount given the set Status Type
+    @type::status_level: ForeignKey(nStatusComponentLevel)
     """
     status_type = models.ForeignKey(nStatusType, on_delete=models.CASCADE)
     status_level = models.ForeignKey(nStatusComponentLevel, on_delete=models.CASCADE)
+
+    ## For fast return of the status level
+    @python_2_unicode_compatible
+    def __str__(self):
+        return status_level.name
+
+    def percentComplete(self):
+
---
+++ noctis/noctis/addonModels.py
@@ -0,0 +1,64 @@
+"""
+Simple additional models taken from across the web.
+- PrettyTrue
+"""
+
+import ast
+import json
+from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
+
+class JSONField(models.TextFeild):
+    """
+    Extracted from the web this is a simple method of extracting
+    valid json data from the database.
+    """
+    __metaclass__ = models.SubfieldBase
+
+    def to_python(self, value):
+        if value == "":
+            return None
+
+        try:
+            if isinstance(value, basestring):
+                return json.loads(value)
+        except ValueError:
+            pass
+        return value
+
+    def get_db_prep_save(self, value, *args, **kwargs):
+        if value == "":
+            return None
+        if isinstance(value, dict):
+            value = json.dumps(value, cls=DjangoJSONEncoder)
+        return super(JSONField, self).get_db_prep_save(value, *args, **kwargs)
+
+class ListField(models.TextField):
+    """
+    Also yanked from the internet. Simple List -> Str -> List
+    for storing data.
+    """
+
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
